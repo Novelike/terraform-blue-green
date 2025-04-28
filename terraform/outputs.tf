d44488b8af3100be317a192671ae74be0125b383
Novelike <kimwogus0130@naver.com>
2025-04-28 15:49:46 +0900

    modified outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,5 @@
-output "blue_ip" {
-  description = "Blue 서버 플로팅 IP"
-  value       = openstack_networking_floatingip_v2.fip["blue"].address
-}
-
-output "green_ip" {
-  description = "Green 서버 플로팅 IP"
-  value       = openstack_networking_floatingip_v2.fip["green"].address
-}
-
-output "lb_vip" {
-  description = "LoadBalancer VIP"
-  value       = openstack_lb_loadbalancer_v2.lb.vip_address
+output "public_ip" {
+  description = "생성된 VM의 퍼블릭(플로팅) IP"
+  value       = openstack_networking_floatingip_v2.public_ip.address
 }
 
